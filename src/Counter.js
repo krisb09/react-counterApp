8893aa51897ac6fb327b67df588d9731d5cc6b70
Kris Bautista <krisb09@gmail.com>
2022-01-07 10:42:57 -0500

    first commit

---
+++ src/Counter.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './counter.css'
+
+function Counter() {
+
+    const [counter, setCounter] = useState(0);
+
+    return (
+      <div className="container">
+        <React.Fragment>
+          <section>
+            <h2>Counter</h2>
+            <h1>{counter}</h1>
+            <button className='btn' onClick={()=> setCounter (counter - 1)}>Decrease</button>
+            <button className='btn' onClick={()=> setCounter(0)}>Reset</button>
+            <button className='btn' onClick={()=> setCounter (counter + 1)}>Increase</button>
+          </section>
+        </React.Fragment>
+      </div>
+    );
+}
+
+export default Counter
